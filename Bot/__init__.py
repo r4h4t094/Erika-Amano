1d9dd9b73033d0f6a9216587250404b762c0fdc0
r4h4t094 <smonarch444@gmail.com>
2025-01-17 17:54:08 +0600

    Update __init__.py
    Signed-off-by: r4h4t094 <smonarch444@gmail.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -11,13 +11,13 @@ LOG.setLevel(level=logging.INFO)
 #<-----------Variables-------------->
 LOG.info('❤️ Checking Bot Variables.....')
 TRIGGERS = os.environ.get("TRIGGERS", "/ !").split(" ")
-BOT_TOKEN = os.environ.get('BOT_TOKEN', 'your bot token') #BOT Token Add
-API_ID = int(os.environ.get('API_ID', 456789)) #Telgram Api id
-APP_HASH = os.environ.get('APP_HASH', 'your api id')# Telgram App hash  
-OWNER_ID = int(os.environ.get('OWNER_ID', 953362604))
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '6392899197:AAGdYygGAZT0gkxxrcjoLOB-HOlief8lZ9I') #BOT Token Add
+API_ID = int(os.environ.get('API_ID', 24720817)) #Telgram Api id
+APP_HASH = os.environ.get('APP_HASH', '43669876f7dbd754e157c69c89ebf3eb')# Telgram App hash  
+OWNER_ID = int(os.environ.get('OWNER_ID', 1235222889)
 MONGO_DB = os.environ.get("MONGO_DB", 'your mongodb') #MONGO DB FOR ANIME DATA
-FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -100456789013)    # Must Fill This ,Add Bot As Admin In Log Channel
-BOT_NAME = os.environ.get('BOT_NAME', 'Soheru')
+FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1002027997392)    # Must Fill This ,Add Bot As Admin In Log Channel
+BOT_NAME = os.environ.get('BOT_NAME', 'shadow_video_encoder_bot')
 #<-----------Variables For 4GB Support (Optional)-------------->
 SESSION_STRING = os.environ.get("SESSION_STRING",'None')  #Replace None With String Session   Your String Session Account Must Be Present In Log Channel
 ubot = None  # Don't Touch This
