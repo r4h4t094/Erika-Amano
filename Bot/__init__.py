90b71bf0b57532524aeae9493a5ed42c6af13a92
r4h4t094 <smonarch444@gmail.com>
2025-01-17 17:56:05 +0600

    Update __init__.py
    Signed-off-by: r4h4t094 <smonarch444@gmail.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -15,7 +15,7 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN', '6392899197:AAGdYygGAZT0gkxxrcjoLOB-HOli
 API_ID = int(os.environ.get('API_ID', 24720817)) #Telgram Api id
 APP_HASH = os.environ.get('APP_HASH', '43669876f7dbd754e157c69c89ebf3eb')# Telgram App hash  
 OWNER_ID = int(os.environ.get('OWNER_ID', 1235222889)
-MONGO_DB = os.environ.get("MONGO_DB", 'your mongodb') #MONGO DB FOR ANIME DATA
+MONGO_DB = os.environ.get("MONGO_DB", 'mongodb+srv://94:94@cluster0.lijgjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') #MONGO DB FOR ANIME DATA
 FILES_CHANNEL = os.environ.get("FILES_CHANNEL", -1002027997392)    # Must Fill This ,Add Bot As Admin In Log Channel
 BOT_NAME = os.environ.get('BOT_NAME', 'shadow_video_encoder_bot')
 #<-----------Variables For 4GB Support (Optional)-------------->
